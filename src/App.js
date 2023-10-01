53e06b57c39834270dc388788377e0e8044ebd5a
arin107 <110413890+arin107@users.noreply.github.com>
2023-10-01 15:16:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Resturant from "./components/Basics/Resturant";
+// import UseState from "./components/Hooks/useState";
+// import UseEffect from "./components/Hooks/useEffect";
+// import UseReducer from "./components/Hooks/useReducer";
+// import Todo from "./components/todoreact/todo";
+// import Temp from "./components/weather/temp";
+
+const App = () => {
+  return <Resturant/>;
+};
+
+export default App;
