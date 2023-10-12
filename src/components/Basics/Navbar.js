9af27baa3c9c6cb6bccd252d27237fe5a6980140
arin107 <arinsrivastava2903@gmail.com>
2023-10-12 12:32:17 +0530

    changed style

--- src/components/Basics/Navbar.js
+++ src/components/Basics/Navbar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 const navbar = ({ filterItem, menuList }) => {
@@ -21,28 +20,4 @@ const navbar = ({ filterItem, menuList }) => {
   );
 };
 
-=======
-import React from "react";
-
-const navbar = ({ filterItem, menuList }) => {
-  return (
-    <>
-      <nav className="navbar">
-        <div className="btn-group">
-          {menuList.map((curElem) => {
-            return (
-              <button
-                className="btn-group__item"
-                onClick={() => filterItem(curElem)}>
-                {curElem}
-              </button>
-            );
-          })}
-        </div>
-      </nav>
-    </>
-  );
-};
-
->>>>>>> 3f7d5f8a6983f97f161435d6fd3145a7a2170c65
 export default navbar;
--- src/components/Basics/Resturant.js
+++ src/components/Basics/Resturant.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import "./style.css";
 import Menu from "./MenuAPI";
@@ -41,48 +40,4 @@ const Resturant = () => {
   );
 };
 
-=======
-import React, { useState } from "react";
-import "./style.css";
-import Menu from "./MenuAPI";
-import MenuCard from "./MenuCard";
-import Navbar from "./Navbar";
-
-const uniqueList = [
-  ...new Set(
-    Menu.map((curElem) => {
-      return curElem.category;
-    })
-  ),
-  "All",
-];
-
-console.log(uniqueList);
-
-const Resturant = () => {
-  const [menuData, setMenuData] = useState(Menu);
-  const [menuList, setMenuList] = useState(uniqueList);
-
-  const filterItem = (category) => {
-    if (category === "All") {
-      setMenuData(Menu);
-      return;
-    }
-
-    const updatedList = Menu.filter((curElem) => {
-      return curElem.category === category;
-    });
-
-    setMenuData(updatedList);
-  };
-
-  return (
-    <>
-      <Navbar filterItem={filterItem} menuList={menuList} />
-      <MenuCard menuData={menuData} />
-    </>
-  );
-};
-
->>>>>>> 3f7d5f8a6983f97f161435d6fd3145a7a2170c65
 export default Resturant;
